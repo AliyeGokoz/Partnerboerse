8b8dce5d9a41047a5eb4ca3fbf4dff83f32ef619
janakuch <janakuch@gmx.de>
2016-05-08 22:57:06 +0200

    Erstellen der Klasse ServersideSettings

--- src/de/hdm/partnerboerse/server/ServersideSettings.java
+++ src/de/hdm/partnerboerse/server/ServersideSettings.java
@@ -1,5 +1,17 @@
 package de.hdm.partnerboerse.server;
 
-public class ServersideSettings {
+import java.util.logging.Logger;
+import de.hdm.partnerboerse.shared.CommonSettings;
+
+public class ServersideSettings extends CommonSettings {
+	private static final String LOGGER_NAME = "Partnerboerse Server";
+	private static final Logger log = Logger.getLogger(LOGGER_NAME);
+	
+	public static Logger getLogger(){
+		return log;
+	}
+			
+		
+	
 
 }
