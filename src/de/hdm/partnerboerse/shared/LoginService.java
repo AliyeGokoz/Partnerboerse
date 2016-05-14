a210bbb8df250eb15fbe791b3a9a7ef2c6a22104
CarolinElsner <dh@mailforu.de>
2016-05-15 00:25:32 +0200

    Erstellen und Füllen der Login-Klassen

--- src/de/hdm/partnerboerse/shared/LoginService.java
+++ src/de/hdm/partnerboerse/shared/LoginService.java
@@ -1,5 +1,15 @@
 package de.hdm.partnerboerse.shared;
 
-public class LoginService {
 
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+ 
+
+ public LoginInfo login(String requestUri);
+ 
 }
