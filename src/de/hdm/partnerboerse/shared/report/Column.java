3224a73c9f37d3ff9019b76b106cd5db8a179b77
Roxana Dinus <rd018@hdm-stuttgart.de>
2016-06-26 18:21:33 +0200

    Kommentare hinzugefügt.

--- src/de/hdm/partnerboerse/shared/report/Column.java
+++ src/de/hdm/partnerboerse/shared/report/Column.java
@@ -2,32 +2,62 @@ package de.hdm.partnerboerse.shared.report;
 
 import java.io.Serializable;
 
+/**
+ * Spalte eines <code>Row</code>-Objekts. <code>Column</code>-Objekte
+ * implementieren das <code>Serializable</code>-Interface. Somit sind Objekte
+ * dieser Klasse über das Netzwerk übertragbar.
+ */
+
 public class Column implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Paragraph value;
 
-
+	/**
+	 * Implementierung eines No-Argument-Konstruktors, um serialisierbare
+	 * Klassen mittels GWT-RPC zu transportieren.
+	 */
 	public Column() {
 	}
-	
-	public Column(Paragraph s){
+
+	/**
+	 * Konstruktor, der die Angabe eines Werts, also einen Spalteneintrag
+	 * erzwingt.
+	 * 
+	 * @param s
+	 *            ist der Wert, der durch das Column-Objekt dargestellt werden
+	 *            soll.
+	 * @see #Column()
+	 */
+
+	public Column(Paragraph s) {
 		this.value = s;
 	}
-	
-	public Paragraph getVaulue(){
+
+	/**
+	 * Der Spaltenwert wird ausgelesen.
+	 */
+
+	public Paragraph getVaulue() {
 		return this.value;
 	}
-	
-	public void setValue(Paragraph newValue){
+
+	/**
+	 * Der aktuelle Spaltenwert wird überschrieben.
+	 * 
+	 * @param newValue
+	 *            ist der neue Spaltenwert
+	 */
+
+	public void setValue(Paragraph newValue) {
 		this.value = newValue;
 	}
 
-//	@Override
-//	public String toString() {
-//		return this.value;
-//	}
-//	
+	// @Override
+	// public String toString() {
+	// return this.value;
+	// }
+	//
 
 }
--- src/de/hdm/partnerboerse/shared/report/Report.java
+++ src/de/hdm/partnerboerse/shared/report/Report.java
@@ -3,43 +3,107 @@ package de.hdm.partnerboerse.shared.report;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * <code>Report</code> ist die Basisklasse aller Reports. Diese Klasse
+ * implementiert das <code>Serializable</code>-Interface, damit Reports vom
+ * Server an den Client gesendet werden können.
+ * 
+ * @see Report
+ */
+
 public class Report implements Serializable {
 
 	public static final long serialVersionUID = 1L;
 
+	// Stellt ein kleines Impressum für die hier realisierten Reports dar.
 	private Paragraph imprint = null;
+
+	// Stellt die Kopfdaten des Berichts dar.
 	private Paragraph headerData = null;
+
+	// (individueller) Titel des Berichts.
 	private String title = "Love24/7";
+
+	// Datum der Erstellung des Berichts.
 	private Date created = new Date();
+
+	/**
+	 * Impressum auslesen.
+	 * 
+	 * @return Text des Impressums
+	 */
 
 	public Paragraph getImprint() {
 		return imprint;
 	}
 
+	/**
+	 * Impressum setzen.
+	 * 
+	 * @param imprint
+	 *            Text des Impressums
+	 */
+
 	public void setImprint(Paragraph imprint) {
 		this.imprint = imprint;
 	}
+
+	/**
+	 * Kopfdaten auslesen.
+	 * 
+	 * @return Text der Kopfdaten.
+	 */
 
 	public Paragraph getHeaderData() {
 		return headerData;
 	}
 
+	/**
+	 * Kopfdaten setzen.
+	 * 
+	 * @param headerData
+	 *            Text der Kopfdaten.
+	 */
+
 	public void setHeaderData(Paragraph headerData) {
 		this.headerData = headerData;
 	}
+
+	/**
+	 * Berichtstitel auslesen.
+	 * 
+	 * @return Titeltext
+	 */
 
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * Berichttitel setzen.
+	 * 
+	 * @param title
+	 *            Titeltext
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * Erstellungsdatum auslesen.
+	 * 
+	 * @return Erstellungsdatum des Berichts
+	 */
 	public Date getCreated() {
 		return created;
 	}
 
+	/**
+	 * Erstellungsdatum setzen.
+	 * 
+	 * @param created
+	 *            , ist der Zeitpunkt der Erstellung
+	 */
 	public void setCreated(Date created) {
 		this.created = created;
 	}
