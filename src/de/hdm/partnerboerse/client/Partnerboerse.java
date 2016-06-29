01f13bfbdd88c7879ca3037123f6f022cd454a05
Aliye Gököz <ag096@hdm-stuttgart.de>
2016-06-29 10:59:50 +0200

    add Comments clean code

--- src/de/hdm/partnerboerse/client/Partnerboerse.java
+++ src/de/hdm/partnerboerse/client/Partnerboerse.java
@@ -7,6 +7,8 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
@@ -37,31 +39,48 @@ public class Partnerboerse implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
+
 		loginService.login(Window.Location.getHref(), new AsyncCallback<LoginInfo>() {
-			
+
 			@Override
 			public void onSuccess(LoginInfo result) {
-				if(result.isLoggedIn()){
+				if (result.isLoggedIn()) {
 					onModuleLoadLoggedIn(result);
 				} else {
 					Window.Location.replace(result.getLoginUrl());
 				}
 			}
-			
+
 			@Override
 			public void onFailure(Throwable caught) {
-				
+
 			}
-		});	
+		});
 
-		
 	}
-	
-	private void onModuleLoadLoggedIn(final LoginInfo loginInfo){
-		final Profile profile = loginInfo.getProfile();
-		final VerticalPanel content = new VerticalPanel();
 
+	/**
+	 * Methode, welche aufgerufen wird, wenn es ein Nutzer mit bestehendem
+	 * Profil ist
+	 * 
+	 * @param loginInfo
+	 */
+	private void onModuleLoadLoggedIn(final LoginInfo loginInfo) {
+		/*
+		 * eingelogtes Profil
+		 */
+		final Profile profile = loginInfo.getProfile();
+		/*
+		 * Vertical Panel für die Ausgabe
+		 */
+		final VerticalPanel all = new VerticalPanel();
+		final VerticalPanel content = new VerticalPanel();
+		final VerticalPanel herzpane = new VerticalPanel();
+
+		/*
+		 * Commands die Aufgerufen werden, wenn in der Menü Leiste ein etwas
+		 * angelickt wird.
+		 */
 		Command addnewProfile = new Command() {
 			public void execute() {
 				NewProfilePage addnewProfil = new NewProfilePage();
@@ -69,11 +88,11 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(addnewProfil);
 			}
 		};
-		
-		if(loginInfo.getProfile() == null){
+
+		if (loginInfo.getProfile() == null) {
 			addnewProfile.execute();
 		}
-		
+
 		Command showProfil = new Command() {
 			public void execute() {
 				ProfilePage showProfil = new ProfilePage();
@@ -81,7 +100,7 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(showProfil.showProfilePage());
 			}
 		};
-		
+
 		Command allUsers = new Command() {
 			public void execute() {
 				UserOverview allUsers = new UserOverview();
@@ -89,7 +108,7 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(allUsers);
 			}
 		};
-		
+
 		Command seeFavoritList = new Command() {
 			public void execute() {
 				FavoritListOverview seeFavoritList = new FavoritListOverview();
@@ -97,7 +116,7 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(seeFavoritList);
 			}
 		};
-		
+
 		Command seeBlockingList = new Command() {
 			public void execute() {
 				BlockingListOverview seeBlockingList = new BlockingListOverview();
@@ -105,7 +124,7 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(seeBlockingList);
 			}
 		};
-		
+
 		Command seeSearchProfilePage = new Command() {
 			public void execute() {
 				SearchProfilePage showSearchProfile = new SearchProfilePage(profile);
@@ -113,53 +132,58 @@ public class Partnerboerse implements EntryPoint {
 				RootPanel.get("Content").add(showSearchProfile);
 			}
 		};
-		
+
 		Command seePartnerProposel = new Command() {
 			public void execute() {
 				Window.Location.replace("PartnerboerseReport.html");
 			}
 		};
-		
+
 		Command logoutUser = new Command() {
 			public void execute() {
 				Window.Location.replace(loginInfo.getLogoutUrl());
 			}
 		};
 
-		// Make some sub-menus that we will cascade from the top menu.
-		
-
-		
-		
+		/*
+		 * Menu-Tabs anlegen
+		 */
 		MenuBar profilMenu = new MenuBar();
-		final String userAddimage = "<img src='images/user_add_edit.png'/>";
-		final String showUserimage ="<img src='images/user.png'/>";
-
-
-		profilMenu.addItem(new MenuItem(userAddimage,true,addnewProfile));
-		profilMenu.addItem(new MenuItem(showUserimage,true,showProfil));
-		
 		MenuBar searchprofilMenu = new MenuBar();
-		final String showSearchprofiles ="<img src='images/searchprofiles.png'/>";
-		//searchprofilMenu.addItem("Suchprofil anlegen", cmd);
-		searchprofilMenu.addItem(new MenuItem(showSearchprofiles,true, seeSearchProfilePage));
+		MenuBar favoritlistMenu = new MenuBar();
+		MenuBar blockedcontactsMenu = new MenuBar();
+		MenuBar allProfilesMenu = new MenuBar();
+		MenuBar partnerproposelMenu = new MenuBar();
+		MenuBar logoutButton = new MenuBar();
 
-		MenuBar favoritlistMenu = new MenuBar(true);
+		/*
+		 * String für das Menü, damit statt einem Text ein Bild angezeigt wird.
+		 */
+		final String userAddimage = "<img src='images/user_add_edit.png'/>";
+		final String showUserimage = "<img src='images/user.png'/>";
+		final String showSearchprofiles = "<img src='images/searchprofiles.png'/>";
+
+		/*
+		 * MenuItems dem Menu-Tab zuweisen. 
+		 */
+		profilMenu.addItem(new MenuItem(userAddimage, true, addnewProfile));
+		profilMenu.addItem(new MenuItem(showUserimage, true, showProfil));
+
+		searchprofilMenu.addItem(new MenuItem(showSearchprofiles, true, seeSearchProfilePage));
+
 		favoritlistMenu.addItem("Merkzettel ansehen", seeFavoritList);
 
-		MenuBar blockedcontactsMenu = new MenuBar(true);
 		blockedcontactsMenu.addItem("Kontaktsperrenliste ansehen", seeBlockingList);
-		
-		MenuBar allProfilesMenu = new MenuBar(true);
+
 		allProfilesMenu.addItem("Alle Profile Ansehen", allUsers);
 
-		MenuBar partnerproposelMenu = new MenuBar(true);
 		partnerproposelMenu.addItem("Finde neue Menschen", seePartnerProposel);
-		
-		MenuBar logoutButton = new MenuBar(true);
+
 		logoutButton.addItem("Loggen Sie sich aus", logoutUser);
 
-		// Make a new menu bar, adding a few cascading menus to it.
+		/*
+		 *  Make a new menu bar, adding a few cascading menus to it.
+		 */
 		MenuBar menu = new MenuBar();
 		menu.addItem("Profil", profilMenu);
 		menu.addItem("Suchprofil", searchprofilMenu);
@@ -168,31 +192,71 @@ public class Partnerboerse implements EntryPoint {
 		menu.addItem("Alle Profile", allProfilesMenu);
 		menu.addItem("Personenvorschläge", partnerproposelMenu);
 		menu.addItem("Logout", logoutButton);
-		//menu.setStyleName("menubar");
 
-		// TODO automatisch Usernamen ausgeben
-		// Say Hello to User
-		final Label lblhello = new Label("Willkommen ");
-
+		/*
+		 * zurgiff auf aktuelles Profil
+		 */
 		loginService.getCurrentProfile(new AsyncCallback<Profile>() {
-			
+
 			@Override
 			public void onSuccess(Profile value) {
-				lblhello.setText("Willkommen " + value.getFirstName());
+				/*
+				 * Strings anlegen für die Ausgabe in der HTML
+				 */
+				final String name = value.getFirstName();
+				final String welcome = "Wilkommen";
+				final String gLiebe = "finde heute deine große ";
+				final String liebe = "Liebe!";
+				final String herz = "<img src='images/herz.png'/>";
+				
+				/*
+				 * HTML anlegen
+				 */
+				final HTML html = new HTML("<div>" + welcome + "</div>");
+				final HTML htmlname = new HTML("<div>" + name + "</div>");
+				final HTML htmltext = new HTML("<div>" + gLiebe + "</div>");
+				final HTML htmlLiebe = new HTML("<div>" + liebe + "</div>");
+				final HTML htmlherz = new HTML("<image>" + herz + "</image>");
+
+				/*
+				 * Widget dem Panel zuweisen
+				 */
+				content.add(html);
+				content.add(htmlname);
+				content.add(htmltext);
+				content.add(htmlLiebe);
+				herzpane.add(htmlherz);
+				
+				/*
+				 * HTML Stylen
+				 */
+				htmlname.setStyleName("username");
+				htmlLiebe.setStyleName("liebetext");
+				htmlherz.setStyleName("herz");
 			}
-			
+
 			@Override
 			public void onFailure(Throwable caught) {
-				
+
 			}
 		});
 
-		// Add it to the root panel.
-
-				content.add(lblhello);
-
-				
-		RootPanel.get("Content").add(lblhello);
+		/*
+		 * Panel stylen
+		 */
+		content.setStyleName("contentpanel");
+		all.setStyleName("contentpanel");
+		
+		/*
+		 * Panels für Text und Bild dem Panel für alle zuweisen
+		 */
+		all.add(content);
+		all.add(herzpane);
+		
+		/*
+		 * Alles dem RootPanel zuweisen
+		 */
+		RootPanel.get("Content").add(all);
 		RootPanel.get("Navigator").add(menu);
 	}
 }
