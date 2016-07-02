38390367e4773f3ce8e8255b531508cc1d7b3162
CarolinElsner <dh@mailforu.de>
2016-07-02 12:58:47 +0200

    Kommentare zu Setting-Klassen hinzugefügt

--- src/de/hdm/partnerboerse/server/ServersideSettings.java
+++ src/de/hdm/partnerboerse/server/ServersideSettings.java
@@ -3,15 +3,18 @@ package de.hdm.partnerboerse.server;
 import java.util.logging.Logger;
 import de.hdm.partnerboerse.shared.CommonSettings;
 
+/**
+ * Beinhaltet alle Konfigurationen die nur auf dem Server gültig sind.
+ * 
+ * @author Carolin Elsner
+ *
+ */
 public class ServersideSettings extends CommonSettings {
 	private static final String LOGGER_NAME = "Partnerboerse Server";
 	private static final Logger log = Logger.getLogger(LOGGER_NAME);
-	
-	public static Logger getLogger(){
+
+	public static Logger getLogger() {
 		return log;
 	}
-			
-		
-	
 
 }
